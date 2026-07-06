6dce835fc9e94aefb2829397f977e80dfb501d16
letter5ivekayo <letter5ivekayo@gmail.com>
2026-07-06 12:58:27 -0500

    Update index.js

--- kayo-bot/index.js
+++ kayo-bot/index.js
@@ -259,6 +259,7 @@ const commands = [
     ],
   },
   {
+  {
   name: 'raffle',
   description: 'Log raffle tickets',
   options: [
@@ -267,7 +268,7 @@ const commands = [
     { name: 'tickets', description: 'How many tickets', type: 4, required: true, min_value: 1 },
   ],
 },
-
+];
 async function registerCommands() {
   const appId = process.env.APPLICATION_ID;
   if (!appId) return;
